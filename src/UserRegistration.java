39c6660b6ced5291a8db29810d1e97e0b4cef023
Jeevanbs <41687022+Jeevanbs@users.noreply.github.com>
2021-04-03 21:52:21 +0530

    User Registration - UC2

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -1,15 +1,31 @@
+import java.util.Scanner;
 import java.util.function.Predicate;
 
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		
+
+
+		Scanner stdin = new Scanner(System.in);
 		//UC1
-		String userName = "Jeevan";
-		String userName1 = "Je";
+		System.out.println("Enter Firstname");
+		String userName = stdin.next();
 		Predicate<String> userNameValidation = n -> Character.isUpperCase(n.charAt(0)) && n.length() > 3;
-		System.out.println("Validation String " + userName + " --- " + userNameValidation.test(userName));
-		System.out.println("Validation String " + userName1 + " --- " + userNameValidation.test(userName1));
+		if(userNameValidation.test(userName)){
+			System.out.println("Valid UserName");
+		}else {
+			System.out.println("InValid UserName - Firstname Has to start with Caps and length shld be greater than 3");
+		}
+
+		//UC2
+		System.out.println("Enter Lastname");
+		String lastName = stdin.next();
+		if(userNameValidation.test(lastName)){
+			System.out.println("Valid lastName");
+		}else {
+			System.out.println("InValid lastName - lastNames Has to start with Caps and length shld be greater than 3");
+		}
+
 	}
 
 }
